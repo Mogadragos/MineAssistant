bc05e1ff31c603950bae555b90146efea513c966
Mogadragos <alice.caillaud17@gmail.com>
2026-04-11 00:15:42 +0200

    Prepare persistence manager

--- src/main/java/com/mogador/mineassistant/managers/PersistenceManager.java
+++ src/main/java/com/mogador/mineassistant/managers/PersistenceManager.java
@@ -1,5 +1,13 @@
 package com.mogador.mineassistant.managers;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.mogador.mineassistant.MineAssistant;
+
 public class PersistenceManager {
 
     // Singleton
@@ -11,5 +19,45 @@ public class PersistenceManager {
         return instance;
     }
     private PersistenceManager() {}
+
+    private MineAssistant plugin;
+    private File syncPowerableFile;
+    private FileConfiguration syncPowerableConfig;
+
+    public void initialize(MineAssistant plugin) {
+        this.plugin = plugin;
+
+        load();
+    }
+
+    public void load() {
+        if (!plugin.getDataFolder().exists()) {
+            plugin.getDataFolder().mkdirs();
+        }
+
+        syncPowerableFile = new File(plugin.getDataFolder(), "levers.yml");
+
+        if (!syncPowerableFile.exists()) {
+            try {
+                syncPowerableFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        syncPowerableConfig = YamlConfiguration.loadConfiguration(syncPowerableFile);
+    }
+
+    public void persist() {
+        try {
+            syncPowerableConfig.save(syncPowerableFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FileConfiguration getData() {
+        return syncPowerableConfig;
+    }
 
 }
